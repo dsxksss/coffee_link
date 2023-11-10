82e318e920986d6192e80edf3ccfe24292db10b6
VentCat <73477803+dsxksss@users.noreply.github.com>
2023-11-10 16:57:22 +0800

    vue-toastification

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,10 +1,19 @@
 <script setup lang="ts">
 import { Button } from '@/components/ui/button'
+import { useToast } from "vue-toastification";
+
+// Get toast interface
+const toast = useToast();
+
+function showToast() {
+  toast("I'm a toast!")
+}
+
 </script>
 
 <template>
   <div>
     <font-awesome-icon icon="fa-solid fa-user-secret" />
-    <Button>Click me</Button>
+    <Button @click="showToast">Click me</Button>
   </div>
 </template>
--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,6 +1,9 @@
-import { createApp } from 'vue'
 import App from './App.vue'
+import { createApp } from 'vue'
 import './assets/index.css'
+import Toast, { PluginOptions } from "vue-toastification";
+// Import the CSS or use your own!
+import "vue-toastification/dist/index.css";
 
 /* import the fontawesome core */
 import { library } from '@fortawesome/fontawesome-svg-core'
@@ -14,6 +17,11 @@ import { faUserSecret } from '@fortawesome/free-solid-svg-icons'
 /* add icons to the library */
 library.add(faUserSecret)
 
+const options: PluginOptions = {
+    // You can set your default options here
+};
+
 createApp(App)
+    .use(Toast, options)
     .component('font-awesome-icon', FontAwesomeIcon)
     .mount('#app')
