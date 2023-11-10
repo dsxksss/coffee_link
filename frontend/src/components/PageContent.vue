d2b501300b0b7c798fc4232b0c0d07ab5e075e00
VentCat <73477803+dsxksss@users.noreply.github.com>
2023-11-10 15:26:22 +0800

    清除前端，重构新前端

--- frontend/src/components/PageContent.vue
+++
@@ -1,9 +0,0 @@
-<template>
-    <!-- 顶部填充物 -->
-    <div class=" h-screen overflow-hidden">
-        <!-- 页面实体内容 -->
-        <div class="h-full overflow-y-scroll scroll-smooth">
-            <RouterView />
-        </div>
-    </div>
-</template>
